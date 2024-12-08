e2ed6f5b8183ccb270f4b888907d6e6406a414a2
Zhen Qi Teo <risotteo@gmail.com>
2024-12-08 13:07:53 +0800

    [Unsubstantial] From React Docs to Practice

---
+++ Documents/artichoke2024/TOP/react/react-1-cv/top-1-cv/src/components/TodoInput.jsx
@@ -0,0 +1,8 @@
+export default function TodoInput() {
+    return (
+        <header>
+            <input placeholder="Enter todo...."/>
+            <button>Add</button>
+        </header>
+    )
+}
